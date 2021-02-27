2ee1537e8dd09092473799aea0259af0ec50c388
Luis Torres <luis.torresmo@alum.uca.es>
2021-02-27 12:22:09 +0100

    SUBIDA INICIAL

---
+++ PROYECTOMP/cmake-build-debug/CMakeFiles/PROYECTOMP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROYECTOMP.dir/main.c.obj"
+  "PROYECTOMP.exe"
+  "PROYECTOMP.exe.manifest"
+  "PROYECTOMP.pdb"
+  "libPROYECTOMP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROYECTOMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
