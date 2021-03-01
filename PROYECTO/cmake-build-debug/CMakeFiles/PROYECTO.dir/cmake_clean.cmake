f9a7c4cb9485e7f4473920e86639b4e251cffa95
Luis Torres <luis.torresmo@alum.uca.es>
2021-03-01 11:09:33 +0100

    PROYECTO INICIAL

---
+++ PROYECTO/cmake-build-debug/CMakeFiles/PROYECTO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROYECTO.dir/main.c.obj"
+  "PROYECTO.exe"
+  "PROYECTO.exe.manifest"
+  "PROYECTO.pdb"
+  "libPROYECTO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROYECTO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- PROYECTOMP/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("Que paso master?");
-    return 0;
-}
