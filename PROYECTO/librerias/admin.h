b3fa1efe9231544b182b53d54c1131bf51159309
EviLAsRz <alvarosantosrom9@gmail.com>
2021-03-06 12:22:29 +0100

    Estructuras y librerias creadas

--- PROYECTO/librerias/admin.h
+++ PROYECTO/librerias/admin.h
@@ -1,8 +1,8 @@
-//
-// Created by as952 on 03/03/2021.
-//
-
 #ifndef PROYECTO_ADMIN_H
 #define PROYECTO_ADMIN_H
+
+#include <iostream>
+#include <stdlib.h>
+#include "core.h"
 
 #endif //PROYECTO_ADMIN_H
---
+++ PROYECTO/librerias/cronista.h
@@ -0,0 +1,8 @@
+#ifndef PROYECTO_CRONISTA_H
+#define PROYECTO_CRONISTA_H
+
+#include <iostream>
+#include <stdlib.h>
+#include "core.h"
+
+#endif //PROYECTO_CRONISTA_H
