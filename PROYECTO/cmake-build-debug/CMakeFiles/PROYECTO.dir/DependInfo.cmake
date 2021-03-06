2d0ccd34937d263af066bfb3ace3b0c7335e9ff3
Mario <mario.martinezdominguez@alum.uca.es>
2021-03-06 11:32:34 +0100

    Leer_fichero_config

--- PROYECTO/cmake-build-debug/CMakeFiles/PROYECTO.dir/DependInfo.cmake
+++ PROYECTO/cmake-build-debug/CMakeFiles/PROYECTO.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/MP/MP2021/PROYECTO/main.c" "D:/MP/MP2021/PROYECTO/cmake-build-debug/CMakeFiles/PROYECTO.dir/main.c.obj"
+  "D:/ESI/CONTENIDO/MP/Liga_cancerigena/PROYECTO/MP2021/PROYECTO/main.c" "D:/ESI/CONTENIDO/MP/Liga_cancerigena/PROYECTO/MP2021/PROYECTO/cmake-build-debug/CMakeFiles/PROYECTO.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
