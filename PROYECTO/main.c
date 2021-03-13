35b374c25c7aa12af8c06b28622ac446e0f3ef20
Mario <mario.martinezdominguez@alum.uca.es>
2021-03-13 12:33:24 +0100

    cambios_main.c

--- PROYECTO/main.c
+++ PROYECTO/main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "librerias/core.h"
 #include "librerias/login.h"
 
@@ -25,11 +26,57 @@ int main() {
     volcar_plantillas(&estructura_plantillas);
     volcar_jugadores_plantillas(&estructura_jugadores_plantillas);
 
+    //VARIABLES_LOCALES
+
+    int i,id;
+    char perfil[15];            //15 es el espacio max del campo usuario_perfil, ya que aquí guardare el perfil del usuario que accede al sistema
+
+    //INICIO
+
+    etiqueta1:
 
     printf("Bienvenid@ a la liga fantastica!!\n\n");
     printf("1) Acceso al sistema (cuenta ya existente)\n");
     printf("2) Registrarse (cuenta no existente)\n\n");
     printf("Selecciona una de las opciones posibles: ");
+    scanf("%i",&i);
+
+    if(i==1){
+
+        id = acceso_sistema(estructura_usuarios);
+
+        if (id ==1){
+
+            goto etiqueta1;
+        }
+
+        for(i=0 ; i<=sizeof(estructura_usuarios) ; i++){
+
+            if(estructura_usuarios[i].usuario_id == id){
+                strcpy(perfil,estructura_usuarios[i].usuario_perfil);
+            }
+        }
+        if (strcmp(perfil,"participante")==0){
+
+            menu_participante(id,estructura_jugadores_plantillas,estructura_futbolistas,estructura_plantillas);
+
+        }
+
+    }
+    
+    else{
+
+        if(i==2){
+
+            registro(estructura_usuarios);
+            goto etiqueta1;
+
+        }
+        else{
+            goto etiqueta1;
+        }
+    }
+
 
     return 0;
 }
