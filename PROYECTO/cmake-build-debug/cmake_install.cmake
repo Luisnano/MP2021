cd8925527640a618ba5c539027c5bf8213e4ecb0
Alejandro <alejandrogm0002@gmail.com>
2021-03-13 12:34:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- PROYECTO/cmake-build-debug/cmake_install.cmake
+++ PROYECTO/cmake-build-debug/cmake_install.cmake
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-# Install script for directory: D:/MP/MP2021/PROYECTO
-=======
 # Install script for directory: C:/Users/aleja/Desktop/MP2021/PROYECTO
->>>>>>> Stashed changes
-=======
-# Install script for directory: D:/ESI/CONTENIDO/MP/Liga_cancerigena/PROYECTO/MP2021/PROYECTO
->>>>>>> master
-=======
-<<<<<<< Updated upstream
-# Install script for directory: C:/Users/Luis Fernando/Desktop/ESI/MP/POXD/MP2021/PROYECTO
-=======
-# Install script for directory: D:/MP/MP2021/PROYECTO
->>>>>>> Stashed changes
->>>>>>> master
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -56,21 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-file(WRITE "D:/MP/MP2021/PROYECTO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-=======
 file(WRITE "C:/Users/aleja/Desktop/MP2021/PROYECTO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> Stashed changes
-=======
-file(WRITE "D:/ESI/CONTENIDO/MP/Liga_cancerigena/PROYECTO/MP2021/PROYECTO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> master
-=======
-<<<<<<< Updated upstream
-file(WRITE "C:/Users/Luis Fernando/Desktop/ESI/MP/POXD/MP2021/PROYECTO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-=======
-file(WRITE "D:/MP/MP2021/PROYECTO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> Stashed changes
->>>>>>> master
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
