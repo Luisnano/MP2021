54750e733a2b85db4937433b79d2795cac5f9a24
Mario <mario.martinezdominguez@alum.uca.es>
2021-03-18 10:26:45 +0100

    Eliminar_plantilla en participante

--- PROYECTO/librerias/participantes.h
+++ PROYECTO/librerias/participantes.h
@@ -121,14 +121,19 @@ void configurar_plantillas(int *id, configuracion *estructura_config, jugadores_
                            equipos *estructura_equipos, usuarios *estructura_usuarios, futbolistas *estructura_futbolistas,
                            plantillas *estructura_plantillas){
 
-    int opcion, i;
+    int opcion, i,plantilla;
 
-    printf("A continuacion, mostraremos tus plantillas actuales. Selecciona una de ellas para configurarla.");
+    printf("A continuacion, mostraremos tus plantillas actuales.");
 
     for(i = 0; i <= sizeof(estructura_plantillas); i++){
-        printf("%i, %s, presupuesto: %i, puntuacion: %i", estructura_plantillas[i].plantilla_id, estructura_plantillas[i].nombre_plantilla,
-               estructura_plantillas[i].presupuesto_disp, estructura_plantillas[i].puntuacion_acum);
+
+        printf("%i, %s, presupuesto: %i, puntuacion: %i", estructura_plantillas[i].plantilla_id,
+               estructura_plantillas[i].nombre_plantilla,estructura_plantillas[i].presupuesto_disp,
+               estructura_plantillas[i].puntuacion_acum);
     }
+
+    printf("Selecciona una de ellas para configurarla:");
+    scanf("%i",&plantilla);
 
     printf("MENU CONFIGURAR-PLANTILLA\n\n");
 
@@ -191,6 +196,126 @@ void listar_jugadores_disponibles(futbolistas *estructura_futbolistas,jugadores_
     }
 
 }
+
+
+void ranking(plantillas *estructura_plantillas) {
+
+    int i, j, aux;
+
+    int ranking[sizeof(estructura_plantillas)];    //PASAMOS LA ESTRUCTURA A UN VECTOR RANKING PARA PODER ASI ORDENARLOS
+
+    for (i = 0; i <= sizeof(estructura_plantillas); i++) {
+
+        ranking[i] = estructura_plantillas[i].puntuacion_acum;
+    }
+
+    //AHORA HACEMOS UN ALGORITMO DE ORDENACION SIMPLE, CON AYUDA DE UN AUX COMPARAMOS SI EL ELEMENTO ACTUAL
+
+    for (i = 0; i <= sizeof(estructura_plantillas); i++) {
+
+        for (j = i; j <= sizeof(estructura_plantillas); j++) {
+
+
+            if (ranking[j] > ranking[j + 1]) {    //ES MAYOR QUE EL SIGUIENTE. SI LO ES, SE INTERCAMBIAN.
+
+            aux = ranking[j + 1];
+
+            ranking[j + 1] = ranking[j];
+
+            ranking[j] = aux;
+
+             }
+         }
+    }
+
+    for(i=0; i <= sizeof(estructura_plantillas); i++){
+
+        printf("%i , %s => %i PUNTOS\n", estructura_plantillas[i].plantilla_id, estructura_plantillas[i].nombre_plantilla, ranking[i]);
+
+    }
+}
+
+void eliminar_plantillas(plantillas *estructura_plantillas, jugadores_plantillas *estructura_jugadores_plantillas){
+
+    int i ,plantilla,v[3];
+    char aux;
+
+    printf("Seleccione el id de su plantilla que desea eliminar , las cuales son:");
+
+    for(i = 0; i <= sizeof(estructura_plantillas); i++){        //Mostramos las plantillas
+
+        printf("%i, %s, presupuesto: %i, puntuacion: %i", estructura_plantillas[i].plantilla_id,
+               estructura_plantillas[i].nombre_plantilla,estructura_plantillas[i].presupuesto_disp,
+               estructura_plantillas[i].puntuacion_acum);
+    }
+
+    printf("Id plantilla:");
+    scanf("%i",&plantilla);
+
+    //Para eliminar la plantilla seleccionada, creamos un vector auxiliar para los datos de formato entero de plantillas
+    //y una variable auxiliar tipo string llamada aux para guardar el plantilla_nombre
+    //Con estos auxiliares guardamos los datos de la plantilla para intercambair su posicion en estructura_plantillas
+    //Con la ultima plantilla, así al hacer el realloc eliminamos la plantilla seleccionada
+
+    for (i=0 ; i<=sizeof(estructura_plantillas) ; i++){
+
+        if(plantilla == estructura_plantillas[i].plantilla_id){     //Para localizar la plantilla introducida
+
+            v[0] = estructura_plantillas[i].usuario_id;
+            v[1] = estructura_plantillas[i].plantilla_id;
+            v[2] = estructura_plantillas[i].presupuesto_disp;
+            v[3] = estructura_plantillas[i].puntuacion_acum;
+            strcpy(aux,estructura_plantillas[i].nombre_plantilla);
+
+            estructura_plantillas[i].usuario_id = estructura_plantillas[sizeof(estructura_plantillas)].usuario_id;
+            estructura_plantillas[i].plantilla_id = estructura_plantillas[sizeof(estructura_plantillas)].plantilla_id;
+            estructura_plantillas[i].presupuesto_disp = estructura_plantillas[sizeof(estructura_plantillas)].presupuesto_disp;
+            estructura_plantillas[i].puntuacion_acum = estructura_plantillas[sizeof(estructura_plantillas)].puntuacion_acum;
+            strcpy(estructura_plantillas[i].nombre_plantilla , estructura_plantillas[sizeof(estructura_plantillas)].nombre_plantilla);
+
+            estructura_plantillas[sizeof(estructura_plantillas)].usuario_id = v[0];
+            estructura_plantillas[sizeof(estructura_plantillas)].plantilla_id = v[1];
+            estructura_plantillas[sizeof(estructura_plantillas)].presupuesto_disp = v[2];
+            estructura_plantillas[sizeof(estructura_plantillas)].puntuacion_acum = v[3];
+            strcpy(estructura_plantillas[sizeof(estructura_plantillas)].nombre_plantilla , aux);
+        }
+    }
+
+    //Redimensionamos el vector estructura_plantillas, al eliminar una plantilla, le quitamos un elemento
+
+    estructura_plantillas = (plantillas*)realloc(estructura_plantillas,(sizeof(estructura_plantillas)-1)*sizeof(int));
+    if(estructura_plantillas==NULL){printf("Fallo de reserva de memoria");}
+
+    //Ahora eliminamos todos los futbolistas de jugadores_plantillas con la id de la plantilla eliminada
+    //El uso del do while se debe a que si cambio un futbolista de la plantilla seleccionada por el ultimo para
+    //Hacer el realloc es posible que el ultimo futbolista tambien pertenezca a esa plantilla
+
+    for(i=0 ; i<=sizeof(estructura_jugadores_plantillas) ; i++){
+
+        do{
+
+            v[0] = estructura_jugadores_plantillas[i].jugador_platilla_id;
+            v[1] = estructura_jugadores_plantillas[i].plantilla_id;
+
+            estructura_jugadores_plantillas[i].jugador_platilla_id =
+                    estructura_jugadores_plantillas[sizeof(estructura_jugadores_plantillas)].jugador_platilla_id;
+            estructura_jugadores_plantillas[i].plantilla_id =
+                    estructura_jugadores_plantillas[sizeof(estructura_jugadores_plantillas)].plantilla_id;
+
+            estructura_jugadores_plantillas[sizeof(estructura_jugadores_plantillas)].jugador_platilla_id = v[0];
+            estructura_jugadores_plantillas[sizeof(estructura_jugadores_plantillas)].plantilla_id = v[1];
+
+            estructura_jugadores_plantillas = (jugadores_plantillas *)
+                    realloc(estructura_jugadores_plantillas,(sizeof(estructura_jugadores_plantillas)-1)*sizeof(int));
+
+            if(estructura_plantillas==NULL){printf("Fallo de reserva de memoria");}
+
+        } while(plantilla == estructura_jugadores_plantillas[i].plantilla_id);
+
+    }
+
+}
+
 
 
 
