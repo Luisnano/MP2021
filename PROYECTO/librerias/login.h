4a1611e7445d54ef73d4fc85d96d2fc0a4348dea
Alejandro <alejandrogm0002@gmail.com>
2021-04-07 17:01:24 +0200

    Corrección llaves else login.h

--- PROYECTO/librerias/login.h
+++ PROYECTO/librerias/login.h
@@ -1,5 +1,5 @@
-#ifndef PROYECTO_LOGIN_H    //Si no esta definido el modulo lo crea
-#define PROYECTO_LOGIN_H    //Definicion del modulo
+#ifndef PROYECTO_LOGIN_H    //Si no está definido el módulo lo crea.
+#define PROYECTO_LOGIN_H    //Definicion del módulo.
 
 //LIBRERIAS
 
@@ -13,12 +13,11 @@
 int acceso_sistema(usuarios **estructura_usuarios, configuracion *estructura_config);
 void registro(usuarios **estructura_usuarios, configuracion *estructura_config);
 
-
 //FUNCIONES
 
 //Función que permitirá a los usuarios registrados acceder al sistema.
 
-//Cabecera: int acceso_sistema(usuarios *estructura_usuarios, configuracion *estructura_config).
+//Cabecera: int acceso_sistema(usuarios **estructura_usuarios, configuracion *estructura_config).
 //Precondición: la función recibe los contenidos de la estructura usuarios y de la estructura configuración.
 //Poscondición: el usuario accede al sistema. En caso de coincidir su usuario y su contraseña, la función devuelve el
 //perfil que le corresponde. En caso de que no esté registrado, devuelve 1.
@@ -26,20 +25,21 @@ void registro(usuarios **estructura_usuarios, configuracion *estructura_config);
 int acceso_sistema(usuarios **estructura_usuarios, configuracion *estructura_config){
 
     int i = 0, j = 0, aux;
-    char u[6], p[9];                    //Mismos tamaños(6 y 9) que el campo nick_usuario/password_usuario en usuario.txt
+    char u[6], p[9];                    //Mismos tamaños(6 y 9) que el campo nick_usuario/password_usuario en usuario.txt.
 
     fflush(stdin);
     printf("\nIntroduzca su usuario: ");
     fgets(u,6,stdin);
     fflush(stdin);
 
-    while(i < estructura_config->tam_usuarios){     //Recorre todo el vector dinamico de usuarios
+    while(i < estructura_config->tam_usuarios){     //Recorre el vector dinámico de usuarios
                                                     //para verificar si existe algun susuario con ese nick.
 
         if(strcmp(u, estructura_usuarios[i]->usuario_nick) == 0){       //Verifica si hay coincidencia.
 
             j = 1;                  //Para afirmar que se ha encontrado coincidencia,
             aux = i;                //guardo en aux el usuario para poder referirme a sus campos mas adelante.
+
         }
 
         i++;
@@ -71,24 +71,22 @@ int acceso_sistema(usuarios **estructura_usuarios, configuracion *estructura_con
 
     }
 
-    else{
-
         printf("\nNo existe ningun usuario con tu nick. Registrate primero.");
         return 1;  //En caso de que no esté registrado el usuario, devuelve 1.
-    }
+        
 }
 
-//Funcion para que los usuarios se puedan registrar.
+//Función para que los usuarios se puedan registrar.
 
-//Cabecera: void registro(usuarios *estructura_usuarios, configuracion *estructura_config).
+//Cabecera: void registro(usuarios **estructura_usuarios, configuracion *estructura_config).
 //Precondición: la función recibe los contenidos de la estructura usuarios y de la estructura configuración.
 //Poscondición: se registra un usuario de tipo participante.
 
 void registro(usuarios **estructura_usuarios, configuracion *estructura_config){
 
 
-    int i,j;        //J y aux serán auxiliares que servirán para saber cuando el nick o la contraseña ya existen
-    char aux[6];    //Tamaño 6 pues el tamaño del campo nick en usuarios es 6
+    int i, j;        //J y aux serán auxiliares que servirán para saber cuando el nick o la contraseña ya existen.
+    char aux[6];    //Tamaño 6 pues el tamaño del campo nick en usuarios es 6.
 
     //Le asignamos un espacio más al vector dinámico usuarios.
 
